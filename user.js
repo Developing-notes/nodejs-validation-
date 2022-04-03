889c8bb68dd39c24e22ff3142b474516113966a9
Tamilanworld <101121374+Tamilanworld@users.noreply.github.com>
2022-04-03 19:50:35 +0530

    models-folder

--- user.js
+++ user.js
@@ -1,64 +1,45 @@
-const {check,validationResult} = require('express-validator');
-const res = require('express/lib/response');
+const mongoose = require('mongoose');
 
-exports.validateUserSignUp = [
-    check('fullname')
-      .trim()
-      .not()
-      .isEmpty()
-      .withMessage('Name is required!')
-      .isString()
-      .withMessage('Must be a valid name!')
-      .isLength({ min: 3, max: 20 })
-      .withMessage('Name must be within 3 to 20 character!'),
-    check('email').normalizeEmail().isEmail().withMessage('Invalid email!'),
-    check('password')
-      .trim()
-      .not()
-      .isEmpty()
-      .withMessage('Password is empty!')
-      .isLength({ min: 8, max: 20 })
-      .withMessage('Password must be 8 to 20 characters long!'), 
-      check('confirmPassword')
-        .trim()
-        .not()
-        .isEmpty()
-        .custom((value, { req }) => {
-        if (value !== req.body.password) {
-            throw new Error('Both password must be same!');
-        }
-        return true;
-        }),
+const userSchema = new mongoose.Schema({
+    fullname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    confirmPassword:{
+        type:String,
+        required:true
+    },
+    phonenumber:{
+        type:Number,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    avatar : Buffer,
+})
 
-      check('phonenumber').trim()
-      .not()
-      .isEmpty().withMessage("phonenumber required!!")
-      .isLength({min:10,max:10}).withMessage("Phonenumber must be 10 digit!"),
-      check('address').trim()
-      .not()
-      .isEmpty().withMessage("Address field required!!")
-      .isLength({min:5,max:10}).withMessage("Address field must have atlease 5 character!"),
+// userSchema.pre('save', function (next) {
+//     if (this.isModified('password')) {
+//       bcrypt.hash(this.password, 8, (err, hash) => {
+//         if (err) return next(err);
+  
+//         this.password = hash;
+//         next();
+//       });
+//     }
+//   });
+  
 
-  ];
 
-  exports.userValidation = (req, res,next) => {
-    const result = validationResult(req).array();
-    if (!result.length) return next();
-
-    const error = result[0].msg;
-    res.json({ success: false, message: error });
-
-    };
-
-    exports.validateUserSignIn = [
-        check('email')
-        .trim()
-        .not()
-        .isEmpty().withMessage('email is required!!')
-        .normalizeEmail().isEmail().withMessage('Invalid email!'),
-        check('password')       
-          .trim()
-          .not()
-          .isEmpty()
-          .withMessage('password is required!'),
-      ];
+module.exports = mongoose.model('User',userSchema)
